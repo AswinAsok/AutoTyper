dcc8eaf9570a70bde8f6b9bfa29b820ec6b42659
Aswin Asok <aswinasokofficial@gmail.com>
2020-09-04 22:16:22 +0530

    Changes Made in Source Code

--- AutoTyper.py
+++ AutoTyper.py
@@ -1,8 +1,10 @@
 import pyautogui, time
 time.sleep(5)
 
-f =open("spamtext.txt",'r')
+f =open("Text.txt",'r')
 
 for word in f:
     pyautogui.typewrite(word)
-    pyautogui.press("enter")
+    time.sleep(0.57)
+    pyautogui.press("space")
+
