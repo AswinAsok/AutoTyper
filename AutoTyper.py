e3b4bf1990c6ac8f24980d3f1505292b2cf3ba5e
Aswin Asok <aswinasokofficial@gmail.com>
2020-09-09 19:33:28 +0530

    Version 2.0

--- AutoTyper.py
+++ AutoTyper.py
@@ -1,10 +1,33 @@
-import pyautogui, time
-time.sleep(5)
+import pyautogui
+import time
+from selenium import webdriver
 
-f =open("Text.txt",'r')
 
-for word in f:
+
+browser = webdriver.Chrome(executable_path='C:/Users/Aswin Asok/chromedriver/chromedriver.exe')
+time.sleep(10)
+browser.get(input("Enter The URL : "))
+
+time.sleep(15)
+
+totalwords = len((browser.find_element_by_xpath("//*[@id='row1']")).find_elements_by_tag_name("span"))
+
+i = 1
+
+while i < totalwords:
+    if i == 1:
+        starttime = time.time()
+        print(starttime)
+
+    word=browser.find_element_by_class_name("highlight").get_attribute("innerHTML")
     pyautogui.typewrite(word)
-    time.sleep(0.57)
+    currenttime = time.time()
     pyautogui.press("space")
+
+    if currenttime - starttime > 60:
+        print(currenttime - starttime)
+        break
+
+    i=i+1
+
 
